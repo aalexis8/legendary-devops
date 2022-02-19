9d5bd266a38980a29b371909f7ba9cba53bb8f51
Alex Alexis <1334629+aalexis8@users.noreply.github.com>
2022-02-19 12:28:29 -0500

    catch all at the bottom, trying to remember

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
 
 app.get('/rain', (req, res) => {
 	res.set('X-full-stack', '4life');
 	res.status(418);
 	res.send('I prefer coffee or some other lame internet joke.');
 })
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
