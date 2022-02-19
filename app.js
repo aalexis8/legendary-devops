c930f5180da71fdb112e1934cdb676d8ce344b7c
Alex Alexis <1334629+aalexis8@users.noreply.github.com>
2022-02-19 12:21:12 -0500

    add rain a new router with response

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+
+app.get('/rain', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('I prefer coffee or some other lame internet joke.');
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
